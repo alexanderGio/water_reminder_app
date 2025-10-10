a601df219b510a7b4c1b5b579ce3522b574e0c0d
giovana <geoalexander2222@gmail.com>
2025-10-10 10:33:10 -0300

    barra de desidratação

---
+++ barra_desidratacao/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePage  // aqui importamos o componente standalone direto
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
