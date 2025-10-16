5baeb5d3c32a09cdff870809ce44e6bd3905cf09
ProjectFAG <151230206+ProjectFAG@users.noreply.github.com>
2025-10-16 11:56:07 -0300

    Design

--- WRapp/src/app/pages/register/register.page.ts
+++ WRapp/src/app/pages/register/register.page.ts
@@ -8,17 +8,12 @@ import { IonicModule, AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-register',
   standalone: true,
+  styleUrls: ['././register.page.scss'],
   imports: [IonicModule, CommonModule, FormsModule],
   template: `
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Criar Conta</ion-title>
-      </ion-toolbar>
-    </ion-header>
-
     <ion-content class="ion-padding">
+    <h1>Cadastro</h1>
       <form (ngSubmit)="onRegister()">
-        <ion-item>
           <ion-input
             [(ngModel)]="email"
             name="email"
@@ -26,9 +21,7 @@ import { IonicModule, AlertController } from '@ionic/angular';
             placeholder="Email"
             required
           ></ion-input>
-        </ion-item>
 
-        <ion-item>
           <ion-input
             [(ngModel)]="password"
             name="password"
@@ -36,10 +29,9 @@ import { IonicModule, AlertController } from '@ionic/angular';
             placeholder="Senha"
             required
           ></ion-input>
-        </ion-item>
 
-        <ion-button expand="block" type="submit">Cadastrar</ion-button>
-        <ion-button expand="block" fill="clear" (click)="goToLogin()"
+        <ion-button class="btn" type="submit">Cadastrar</ion-button>
+        <ion-button class="btn" fill="clear" (click)="goToLogin()"
           >Voltar</ion-button
         >
       </form>
